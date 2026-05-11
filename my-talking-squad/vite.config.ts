2bedf36d4d8073622c9f8bbe7d3e7395d0445758
keshavrana456 <keshavrana2911@gmail.com>
2026-05-11 12:40:23 +0530

    Update vite.config.ts

--- my-talking-squad/vite.config.ts
+++ my-talking-squad/vite.config.ts
@@ -12,5 +12,11 @@ export default defineConfig({
     },
   },
 
-  assetsInclude: ['**/*.mpeg', '**/*.mp3', '**/*.wav'],
+  assetsInclude: ['**/*.mpeg'],
+
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
 })
