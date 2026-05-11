848e28f9eded5bc3a08d7e2e86c0aaee87648162
keshavrana456 <keshavrana2911@gmail.com>
2026-05-11 12:16:40 +0530

    Update vite.config.ts

--- my-talking-squad/vite.config.ts
+++ my-talking-squad/vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -8,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './attached_assets'),
     },
   },
 })
